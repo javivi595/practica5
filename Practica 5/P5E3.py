a53e678dfc23bde7854f18863874e88b590a733c
javivi595 <javivi595@hotmail.com>
2019-10-15 20:34:41 +0200

    Update P5E3.py

--- Practica 5/P5E3.py
+++ Practica 5/P5E3.py
@@ -1,4 +1,18 @@
 #P5E3 JAVIER DURAN Pide dos numeros y imprime el sumatorio de todos los numeros intermedios
 
-a, b=map(int,input("Introduce dos numeros: ").strip().split())
-print("La suma de todos los numeros intermedios es {}.".format(sum(range(min(a,b), max(a,b)+1))))
+#Forma rapida
+#a, b=map(int,input("Introduce dos numeros: ").strip().split())
+#print("La suma de todos los numeros intermedios es {}.".format(sum(range(min(a,b), max(a,b)+1))))
+
+#Forma con for
+
+a, b=map(int,input("Introduce dos numeros para hacer sumatorio de su rango: ").strip().split())
+suma=0
+
+if a<b:
+    for c in range(a,b+1):
+        suma+=c
+else:
+    for c in range(b,a+1):
+        suma+=c
+print("El sumatorio total es", suma)
